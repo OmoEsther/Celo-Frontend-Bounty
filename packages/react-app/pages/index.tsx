348920486a08f3b5d4a080105f6555589d034aa0
OmoEsther <estyomojo@gmail.com>
2023-10-16 10:41:14 +0100

    final touches

--- packages/react-app/pages/index.tsx
+++ packages/react-app/pages/index.tsx
@@ -1,11 +1,13 @@
 // This is the main page of the app
 import RoomList from "@/components/RoomList";
 import AddRoomModal from "@/components/modal/AddRoomModal";
+import NoticeBoard from "@/components/NoticeBoard";
 
 // Export the Home component
 export default function Home() {
   return (
     <div className="w-full">
+      <NoticeBoard />
       <AddRoomModal />
       <RoomList />
     </div>
